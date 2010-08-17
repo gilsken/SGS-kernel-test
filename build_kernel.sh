e3facf563215070b57ea126edef445d3ab42e089
glisken <duphong@free.fr>
2010-08-18 00:01:31 +0200

    commit build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,43 +1,32 @@
 #/bin/bash
 
-echo "$1 $2 $3"
-
-case "$1" in
-	Clean)
-		echo "********************************************************************************"
-		echo "* Clean Kernel                                                                 *"
-		echo "********************************************************************************"
-
-		pushd linux-2.6.29
-		make clean
-		popd
-		pushd modules
-		make clean
-		popd
-		echo " It's done... "
-		exit
-		;;
-	*)
-		PROJECT_NAME=aries
-		HW_BOARD_REV="03"
-		;;
-esac
-
 if [ "$CPU_JOB_NUM" = "" ] ; then
 	CPU_JOB_NUM=8
 fi
 
-TOOLCHAIN=`pwd`/../arm-2009q3/bin/
-TOOLCHAIN_PREFIX=arm-none-eabi-
+#TOOLCHAIN=`pwd`/../arm-2010q1/bin/
+#TOOLCHAIN_PREFIX=arm-none-eabi-
+
+TOOLCHAIN=/home/phong/GT-I9000_OpenSource/GalaxyS/arm-eabi-4.2.1/bin/
+TOOLCHAIN_PREFIX=arm-eabi-
+
 
 KERNEL_BUILD_DIR=linux-2.6.29
+MODULES_BUILD_DIR=modules
+PROJECT_NAME=aries
+HW_BOARD_REV="03"
+
+#TODO
+#add to arch/arm/config/aries_rev03_defconfig
+#CONFIG_BLK_DEV_INITRD=y
+#CONFIG_INITRAMFS_SOURCE="../directory_initramfs"
+#CONFIG_INITRAMFS_ROOT_UID=0
+#CONFIG_INITRAMFS_ROOT_GID=0
 
 export PRJROOT=$PWD
 export PROJECT_NAME
 export HW_BOARD_REV
-
 export LD_LIBRARY_PATH=.:${TOOLCHAIN}/../lib
-
 echo "************************************************************"
 echo "* EXPORT VARIABLE		                            	 *"
 echo "************************************************************"
@@ -46,17 +35,30 @@ echo "PROJECT_NAME=$PROJECT_NAME"
 echo "HW_BOARD_REV=$HW_BOARD_REV"
 echo "************************************************************"
 
+BUILD_CONFIG()
+{
+	echo "************************************************************"
+	echo "* BUILD_CONFIG                              	 *"
+	echo "************************************************************"
+	echo
+	pushd $KERNEL_BUILD_DIR
+	export KDIR=`pwd`
+	make ARCH=arm $PROJECT_NAME"_rev"$HW_BOARD_REV"_defconfig"
+	popd
+}
+
+
 BUILD_MODULE()
 {
 	echo "************************************************************"
 	echo "* BUILD_MODULE	                                       	 *"
 	echo "************************************************************"
-	echo
-
-	pushd modules
-
-		make ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
-
+	echo $1 $2
+	pushd $KERNEL_BUILD_DIR
+	export KDIR=`pwd`
+	popd
+	pushd $MODULES_BUILD_DIR
+	make $1 $2 ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
 	popd
 }
 
@@ -67,26 +69,39 @@ BUILD_KERNEL()
 	echo "************************************************************"
 	echo
 
-
 	pushd $KERNEL_BUILD_DIR
-
-	export KDIR=`pwd`
-
-	make ARCH=arm $PROJECT_NAME"_rev"$HW_BOARD_REV"_defconfig"
-
-	# make kernel
-	make -j$CPU_JOB_NUM HOSTCFLAGS="-g -O2" ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
-
+	make HOSTCFLAGS="-g -O2" ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX $2 $3
+	#make -j$CPU_JOB_NUM HOSTCFLAGS="-g -O2" ARCH=arm CROSS_COMPILE=$TOOLCHAIN/$TOOLCHAIN_PREFIX
 	popd
-	
-	BUILD_MODULE
 }
 
-# print title
+BUILD_CLEAN()
+{
+	echo "********************************************************************************"
+	echo "* Clean Kernel                                                                 *"
+	echo "********************************************************************************"
+	pushd $KERNEL_BUILD_DIR
+	make clean
+	popd
+	pushd $MODULES_BUILD_DIR
+	make clean
+	popd
+}
+
+BUILD_MENUCONFIG()
+{
+	echo "********************************************************************************"
+	echo "* menuconfig                                                                   *"
+	echo "********************************************************************************"
+	pushd $KERNEL_BUILD_DIR
+	make menuconfig
+	popd
+}
+
 PRINT_USAGE()
 {
 	echo "************************************************************"
-	echo "* PLEASE TRY AGAIN                                         *"
+	echo "* build_kernel [config|kernel|module|menuconfig|clean]    *"
 	echo "************************************************************"
 	echo
 }
@@ -101,6 +116,38 @@ PRINT_TITLE()
 	echo "* 2. modules"
 	echo "************************************************************"
 }
+
+
+echo "$1 $2 $3"
+
+case "$1" in
+	clean)
+		BUILD_CLEAN
+		echo " It's done... "
+		exit
+		;;
+	config)
+		BUILD_CONFIG
+		echo " It's done... "
+		exit
+		;;
+	kernel)
+		BUILD_KERNEL $2 $3
+		echo " It's done... "
+		exit
+		;;
+	module)
+		BUILD_MODULE $2 $3
+		echo " It's done... "
+		exit
+		;;
+	menuconfig)
+		BUILD_MENUCONFIG
+		echo " It's done... "
+		exit
+		;;
+	
+esac
 
 ##############################################################
 #                   MAIN FUNCTION                            #
@@ -118,7 +165,10 @@ START_TIME=`date +%s`
 
 PRINT_TITLE
 
+BUILD_CONFIG
 BUILD_KERNEL
+BUILD_MODULE
+
 END_TIME=`date +%s`
 let "ELAPSED_TIME=$END_TIME-$START_TIME"
 echo "Total compile time is $ELAPSED_TIME seconds"
